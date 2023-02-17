e066d7ed8226467c69fbd4a46fcaa7de12be1231
Branden Ge <86636108+brandenge@users.noreply.github.com>
2023-02-17 17:15:32 -0600

    Test: add tests for #initialize_board and #render_board

--- lib/board.rb
+++ lib/board.rb
@@ -1,6 +1,18 @@
 class Board
   def initialize
+    @grid = initialize_board
+  end
 
-    
+  def initialize_board
+    board = []
+    6.times { board << ('.' * 7).chars }
+    board
+  end
+
+  def render_board
+    @grid.each { |row| puts row.join(' ') }
   end
 end
+
+board = Board.new
+board.render_board
