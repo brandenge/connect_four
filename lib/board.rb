82cd15d7a78bfd86415a2d98bf1f5399c995b1b1
Branden Ge <86636108+brandenge@users.noreply.github.com>
2023-02-18 07:40:31 -0600

    Add colors to board

--- lib/board.rb
+++ lib/board.rb
@@ -1,21 +1,26 @@
 class Board
+  COLORS = {
+    green: '[0;1;30;90;46m [0m',
+    white: '[0;37;5;47;107m [0m',
+    red: '[0;31;5;41;101m [0m'
+  }.freeze
+
   attr_reader :grid
   def initialize
-    @grid = initialize_board
+    @grid = []
   end
 
   def initialize_board
-    board = []
-    6.times { board << ('.' * 7).chars }
-    board
+    @grid = [*(1..6)].map do |row|
+      [*(1..7)].map do |col|
+        (COLORS[:white])
+      end
+    end
   end
 
   def render_board
-    puts [*('A'..'G')].join(" ")
-    @grid.each { |row| puts row.join(' ') }
+    puts [*('A'..'G')].join
+    @grid.each { |row| puts row.join }
     @grid
   end
 end
-
-board = Board.new
-board.render_board
--- lib/game.rb
+++ lib/game.rb
@@ -1,14 +1,34 @@
 require './lib/title'
+require './lib/board'
 
 class Game
   include Title
 
   def initialize
-
+    @board = Board.new
   end
 
   def start
+    print_welcome
+    @board.initialize_board
+    @board.render_board
+  end
+
+  def print_welcome
     puts TITLE
+    puts <<-WELCOME
+
+      **************************************************************************
+
+          WELCOME TO CONNECT 4
+
+      **************************************************************************
+
+          Please enter p to play. Or enter q to quit.
+
+      **************************************************************************
+
+    WELCOME
   end
 end
 
