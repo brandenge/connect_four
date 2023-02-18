1c5c602451f7f76484df9c19d6f78cd00bb36121
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-02-17 20:27:31 -0700

    Feat: modify player class.

--- lib/player.rb
+++ lib/player.rb
@@ -1,22 +1,6 @@
 class Player
 
-  @@count = 0
-
-  def initialize
+  def initialize (name)
     @name = get_name
-    @symbol = get_symbol
   end
-
-  def get_name
-    @@count += 1
-    puts "Player #{@@count}, enter your name"
-    name = gets.chomp
-  end
-
-  def get_symbol
-    symbol = @@count==1 ? "X" : "O"
-    puts "Your symbol is #{symbol}"
-    symbol
-  end
-
 end
