c5de48a101950bbea8ce9ba4f9554b5d2b29db95
Branden Ge <86636108+brandenge@users.noreply.github.com>
2023-02-19 14:01:51 -0600

    Test: add tests for Board class

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -42,27 +42,184 @@ RSpec.describe Board do
     end
   end
 
+  describe 'COLORS constant' do
+    it 'has a colors constant for the possible colors in the grid' do
+      expect(Board::COLORS).to eq(COLORS)
+    end
+  end
+
+  describe '@@columns' do
+    it 'has a columns class variable for storing an array of columns' do
+      it 'is an array of all of the columns of the board/grid' do
+        expect(Board.columns).to eq([*('A'..'G')])
+      end
+    end
+  end
+
   describe '#grid' do
     it 'has a grid' do
-      expect(@board.grid).to eq([])
+      expect(@board.grid).to eq(INITIAL_GRID)
+    end
+  end
+
+  describe '#turns' do
+    it 'starts empty' do
+      expect(@board.turns.empty?).to eq(true)
+    end
+
+    it 'returns an array of Turn objects' do
+      @board.next_turn(@player_1)
+      @board.next_turn(@player_2)
+      actual = @board.next_turn(@player_1).all? { |turn| turn.is_a?(Turn) }
+      expect(actual).to eq(true)
     end
   end
 
   describe '#initialize_board' do
-    it 'modifies the @grid' do
-      @board.initialize_board
-      expect(@board.grid).to eq(INITIAL_GRID)
-    end
-
     it 'returns the initial grid' do
       expect(@board.initialize_board).to eq(INITIAL_GRID)
     end
   end
 
-  describe '#render_board' do
+  describe '#render' do
     it 'returns the updated grid' do
-      @board.initialize_board
-      expect(@board.render_board).to eq(INITIAL_GRID)
+      expect(@board.render).to eq(INITIAL_GRID)
+    end
+  end
+
+  describe '#next_turn' do
+    it 'returns an array of Turn objects' do
+      expect(@board.next_turn(@player_1))
+    end
+
+    it 'adds a Turn object to the array of turns' do
+      @board.next_turn(@player_1)
+      expect(@board.turns.count).to eq(1)
+      @board.next_turn(@player_2)
+      expect(@board.turns.count).to eq(2)
+      @board.next_turn(@player_1)
+      expect(@board.turns.count).to eq(3)
+    end
+  end
+
+  describe '#update' do
+    it 'does nothing at the start' do
+      @board.update
+      expect(@board.grid).to eq(INITIAL_GRID)
+    end
+
+    it 'returns nil' do
+      expect(@board.update).to eq(nil)
+    end
+
+    it 'it updates the board after 1 turn' do
+      mock_turn(@player_1.token, 'A')
+      @board.set_turns([@turn_1])
+      @board.update
+      expect(@board.grid).to eq(@mock_grid)
+    end
+
+    it 'it updates the board after 2 turns' do
+      mock_turn(@player_1.token, 'A')
+      mock_turn(@player_2.token, 'B')
+      @board.set_turns([@turn_1, @turn_2])
+      @board.update
+      expect(@board.grid).to eq(@mock_grid)
+    end
+
+    it 'it updates the board after 3 turns' do
+      mock_turn(@player_1.token, 'A')
+      mock_turn(@player_2.token, 'B')
+      mock_turn(@player_1.token, 'A')
+      @board.set_turns([@turn_1, @turn_2, @turn_3])
+      @board.update
+      expect(@board.grid).to eq(@mock_grid)
+    end
+
+    it 'updates the board after many turns' do
+      mock_turn(@player_1.token, 'A')
+      mock_turn(@player_2.token, 'B')
+      mock_turn(@player_1.token, 'A')
+      mock_turn(@player_2.token, 'B')
+      mock_turn(@player_1.token, 'A')
+      mock_turn(@player_2.token, 'B')
+      mock_turn(@player_1.token, 'A')
+      @board.set_turns([@turn_1, @turn_2, @turn_3, @turn_4, @turn_5, @turn_6, @turn_7])
+      @board.update
+      expect(@board.grid).to eq(@mock_grid)
+    end
+  end
+
+  describe '#find_lowest_row' do
+    it 'defaults to the last row index' do
+      expect(@board.find_lowest_row('A')).to eq(5)
+    end
+
+    it 'returns the second-to-last row index when there is a token in the same column' do
+      @board.set_turns([@turn_1])
+      @board.update
+      expect(@board.find_lowest_row('A')).to eq(4)
+    end
+
+    it 'returns the fourth row index when there are 2 tokens in the same column' do
+      @board.set_turns([@turn_1, @turn_3])
+      @board.update
+      expect(@board.find_lowest_row('A')).to eq(3)
+    end
+
+    it 'returns the third row index when there are 3 tokens in the same column' do
+      @board.set_turns([@turn_1, @turn_3, @turn_5])
+      @board.update
+      expect(@board.find_lowest_row('A')).to eq(2)
+    end
+
+    it 'returns the second row index when there are 4 tokens in the same column' do
+      @board.set_turns([@turn_1, @turn_3, @turn_5, @turn_7])
+      @board.update
+      expect(@board.find_lowest_row('A')).to eq(1)
+    end
+
+    it 'returns the first row index when there are 5 tokens in the same column' do
+      @board.set_turns([@turn_1, @turn_3, @turn_5, @turn_7, @turn_9])
+      @board.update
+      expect(@board.find_lowest_row('A')).to eq(0)
+    end
+  end
+
+  describe '#valid_columns' do
+    it 'starts with all columns being valid' do
+      expect(@board.valid_columns).to eq(@board.columns)
+    end
+
+    it 'it removes full columns from the array of valid columns' do
+      @board.set_turns([@turn_1, @turn_3, @turn_5, @turn_7, @turn_9, @turn_11])
+      @board.update
+      expect(@board.valid_columns).to eq([*('B'..'G')])
+    end
+
+    it 'returns an empty array when there are no columns valid' do
+      @mock_grid = [*(1..6)].map do |row|
+        [*(1..7)].map { |col| COLORS[:blue] }
+      end
+      expect(@board.valid_columns).to eq([])
+    end
+  end
+
+  describe '#winner' do
+    it 'returns nil when there is no winner' do
+      expect(@board.winner).to eq(nil)
+    end
+
+    it 'returns the winning player\'s color when they have won' do
+      @board.set_turns([@turn_1, @turn_2, @turn_3, @turn_4, @turn_5, @turn_6, @turn_7])
+      @board.update
+      expect(@board.winner).to eq(:blue)
+    end
+
+    it 'returns a different winning player\'s color when they have won' do
+      @board.set_turns([@turn_1, @turn_2, @turn_4, @turn_5, @turn_6, @turn_7, @turn_8])
+      @board.update
+      expect(@board.winner).to eq(:red)
     end
   end
 end
