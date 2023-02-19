7de415a8e95b4702941f754c89911478388b1b18
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-02-19 13:55:03 -0700

    Feat: add methods.

--- lib/player.rb
+++ lib/player.rb
@@ -1,8 +1,10 @@
 class Player
-  attr_reader :name
+  attr_reader :name, 
+              :token
 
-  def initialize (name, is_human = true)
+  def initialize (name, token, is_human = true)
     @name = name
+    @token = token
     @is_human = is_human
   end
 
@@ -15,10 +17,19 @@ class Player
   end
 
   def player_move(valid_columns)
-
+    puts "#{@name}, please select letter to use as a drop point from the following columns: #{valid_columns.join(", ")}."
+    player_selection = gets.chomp.upcase
+    unless valid_columns.include?(player_selection)
+      puts "Sorry, that is not a valid selection, please choose from one of the following: #{valid_columns.join(", ")}."
+      player_selection = gets.chomp
+    end
+    player_selection
   end
 
   def random_move(valid_columns)
-
+    valid_columns.sample
   end
 end
+
+
+
