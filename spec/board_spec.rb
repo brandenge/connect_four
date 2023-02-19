11b3d24101fe286a91e2dbb1a5302e0dc1a036a5
Branden Ge <86636108+brandenge@users.noreply.github.com>
2023-02-19 14:01:32 -0600

    Test: add test setup for Board class tests

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -3,6 +3,37 @@ require 'spec_helper'
 RSpec.describe Board do
   before(:each) do
     @board = Board.new
+
+    @player_1 = Player.new(*PLAYER_1.values)
+    @player_2 = Player.new(*PLAYER_2.values)
+
+    @turn_1 = Turn.new(@player_1, 'A')
+    @turn_2 = Turn.new(@player_2, 'B')
+    @turn_3 = Turn.new(@player_1, 'A')
+    @turn_4 = Turn.new(@player_2, 'B')
+    @turn_5 = Turn.new(@player_1, 'A')
+    @turn_6 = Turn.new(@player_2, 'B')
+    @turn_7 = Turn.new(@player_1, 'A')
+    @turn_8 = Turn.new(@player_2, 'B')
+    @turn_9 = Turn.new(@player_1, 'A')
+    @turn_10 = Turn.new(@player_2, 'B')
+    @turn_11 = Turn.new(@player_1, 'A')
+
+    def @board.columns
+      @columns
+    end
+
+    def @board.set_turns(turns)
+      @turns = turns
+    end
+
+    @mock_grid = [*(1..6)].map do |row|
+      [*(1..7)].map { |col| COLORS[:white] }
+    end
+
+    def mock_turn(token, column)
+      @mock_grid[find_lowest_row(column)][@board.columns.find(column)] = token
+    end
   end
 
   describe '#initialize' do
