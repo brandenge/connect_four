7454a3582fca5f60347f6cbfeb7464aa755417f7
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-02-19 21:59:16 -0700

    Feat: add game menu and winning message.

--- lib/game.rb
+++ lib/game.rb
@@ -12,8 +12,8 @@ class Game
   end
 
   def start
-    print_welcome
-    play_game
+    render_welcome
+    render_menu
   end
 
   def play_game
@@ -31,13 +31,20 @@ class Game
         @board.render
       end
     end
+    if @board.valid_columns.empty?
+     puts "Wow, its a draw... try again!"
+    else
+      puts "..."
+      sleep(1)
+     puts "Congratulations #{@board.winner.name}, you have won!!!"
+    end
   end
 
   def game_over?
     @board.winner || @board.valid_columns.empty?
   end
 
-  def print_welcome
+  def render_welcome
     puts TITLE
     puts <<-WELCOME
 
@@ -47,10 +54,20 @@ class Game
 
       **************************************************************************
 
-          Please enter p to play, or q to quit.
-
-      **************************************************************************
-
     WELCOME
+  end
+
+  def render_menu
+    puts "\n        please enter 'p' to play or enter 'q' to quit"
+    user_input = gets.chomp
+    until user_input == 'p' || user_input == 'q'
+        "please enter one of the following: p or q"
+        user_input = gets.chomp
+    end
+    if user_input == "p" 
+      play_game
+    elsif user_input == "q"
+      Process.exit!
+    end
   end
 end
